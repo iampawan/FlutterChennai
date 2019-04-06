7a2d43159c3094a71249123be02dea0f6f946639
Pawan Kumar <pawan221b@gmail.com>
2019-04-06 13:30:37 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Flutter Chennai",
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "GDG Chennai",
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "Welcome to GDG Chennai",
+          textAlign: TextAlign.center,
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text(
+                "mtechviral@gmail.com",
+              ),
+              accountName: Text("Pawan Kumar"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://secure.meetupstatic.com/photos/event/8/6/3/d/600_462394365.jpeg"),
+              ),
+            ),
+            ListTile(
+              title: Text("Option 1"),
+              subtitle: Text("This is option 1"),
+              leading: CircleAvatar(
+                child: Text("1"),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+            ),
+            ListTile(
+              title: Text("Option 2"),
+              subtitle: Text("This is option 2"),
+              leading: CircleAvatar(
+                child: Text("2"),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+            ),
+            ListTile(
+              title: Text("Option 3"),
+              subtitle: Text("This is option 3"),
+              leading: CircleAvatar(
+                child: Text("3"),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
